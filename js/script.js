c069b2f44d871654b776d6e1ecc8b5661cb6bb7a
AbijaK <abijamk@gmail.com>
2022-06-29 09:33:18 +0200

    Refactor: edit function to validate Firstname, Lastname and Email

--- js/script.js
+++ js/script.js
@@ -1,6 +1,76 @@
 let form = document.querySelector('#loginForm');
 
+/*
+    test if form is not empty 
+    =========================
+ */ 
+// add listener on form
+
+
+/*
+    input Firstname traitement
+    ==========================
+ */ 
 // add listener for Firstname
+form.firstname.addEventListener('change', function() {
+   validFirstname(this);
+})
+
+// Create function to valide Firstname
+const validFirstname = function(inputFirstname) {
+    
+    // Create RegExp for Firstname validation
+    let firstnameRegExp = new RegExp('^[a-zA-Z-\s]+$', 'g');
+    
+    let testFirstname = firstnameRegExp.test(inputFirstname.value); 
+
+    // Catch the 'small' tag
+    let small = inputFirstname.nextElementSibling;
+    const smallStyle = document.querySelector('div');
+
+    if (!testFirstname) {
+        small.innerHTML = 'Looks like this cannot be an Firstname';
+        small.style.color = 'red';
+    }else if (inputFirstname.value.trim() == '') {
+        small.innerHTML = 'Firstname cannot be empty';
+        small.style.color = 'red';
+    }
+}
+
+/*
+    input Lastname traitement
+    ==========================
+*/ 
+
+// add listener for Lastname
+form.lastname.addEventListener('change', function () {
+    validLastname(this);
+})
+
+// Create RegExp for Lastname validation
+const validLastname = function(inputLastname) {
+    let lastnameRegExp = new RegExp('^[a-zA-Z-\s]+$', 'g');
+    
+    let testLastname = lastnameRegExp.test(inputLastname.value); 
+
+    // Catch the 'small' tag
+    let small = inputLastname.nextElementSibling;
+    const smallStyle = document.querySelector('div');
+    
+    if (!testLastname) {
+        small.innerHTML = 'Looks like this cannot be an Lastname';
+        small.style.color = 'red';
+    }else if (inputLastname.value.trim() == '') {
+        small.innerHTML = 'Lastname cannot be empty';
+        small.style.color = 'red';
+    }
+}
+
+/*
+    input Email traitement
+    ======================
+ */ 
+// add listener for Email
 form.email.addEventListener('change', function() {
     validEmail(this);
 })
@@ -17,16 +87,10 @@ const validEmail = function(inputEmail) {
     let small = inputEmail.nextElementSibling;
     const smallStyle = document.querySelector('div');
 
-    if (testEmail) {
-        smallStyle.style.display = 
-        small.innerHTML = 'Adresse valide';
-
-    }else{
+    if (!testEmail) {
         small.innerHTML = 'Looks like this cannot be an email';
+        small.style.color = 'red';
     }
 }
-// add listener for Lastname
-
-// add listener for Email
 
 // add listener for Password
